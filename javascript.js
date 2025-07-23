847724d913cd96df3ebf378b77944388732defa2
PeasLeader <211513404+PeasLeader@users.noreply.github.com>
2025-07-23 21:35:05 +0530

    every button is now functional

--- javascript.js
+++ javascript.js
@@ -6,7 +6,11 @@ let symbol = document.querySelectorAll(".symbl")
 let history = document.querySelector(".history")
 let result = document.querySelector(".result")
 let remove = document.querySelector(".dlt")
-let currentOperator = null
+let finalsum = null
+let preValue = null
+let currentValue =null
+let operate
+
 
 
 number.forEach((item)=>{
@@ -22,6 +26,8 @@ number.forEach((item)=>{
             let store = print.textContent
             word.innerText += store
             history.appendChild(word)
+            currentValue = +print.innerText
+            preValue = null
         })
     } else if (item.innerText === "."){
       item.addEventListener("click",(e)=>{
@@ -33,71 +39,53 @@ number.forEach((item)=>{
                 print.innerText += store
         }
       })
-    }else {        
-        // console.log(item)
+    } else {        
         item.addEventListener("click",(e)=>{
-          let store = e.target.textContent
-          print.textContent += store.trim()
+          let store = e.target.textContent 
+          if (currentValue){
+            print.textContent += store.trim()
+             currentValue = +print.innerText
+          } else {
+          print.textContent = store.trim()
+          currentValue = +print.innerText
+          }
         })
     }
-
    
     })
 
-let arr = []
 
 removeAll.addEventListener("click",()=>{
     print.textContent = ""
-    arr.length = 0
-    console.log(arr)
+    preValue = null;
+    currentValue = null;
+    operate = null;
+    finalsum = null
 })
 
 remove.addEventListener("click",(e)=>{
     print.textContent = print.textContent.slice(0, -1);
+    currentValue = +print.innerText
 })
+
 
 
 symbol.forEach((item)=>{
     item.addEventListener("click",(e)=>{
-      let  word = document.createElement("div")
-        word.className = "word"
-        let store = print.textContent
-        word.innerText += store
-        history.appendChild(word)
-        print.textContent = ""
-        if (e.target.textContent === "×"){
-            currentOperator = e.target.textContent
-          let words =  word.innerText
-           arr.push(words)
-            let Arr =  arr.filter((e)=>{
-                   return e
-              })
-           console.log(Arr)
-        } else if (e.target.textContent === "+"){
-            currentOperator = e.target.textContent
-            let words =   word.innerText
-            arr.push(words)
-              let Arr =  arr.filter((e)=>{
-                   return e
-              })
-              console.log(Arr)
-        } else if (e.target.innerText ==="-"){
-            currentOperator = e.target.textContent
-            let words =   word.innerText
-            arr.push(words)
-              let Arr =  arr.filter((e)=>{
-                   return e
-              })
-              console.log(Arr)
-        } else if (e.target.innerText ==="÷"){
-            currentOperator = e.target.textContent
-            let words =   word.innerText
-            arr.push(words)
-              let Arr =  arr.filter((e)=>{
-                   return e
-              })
+        if (operate){
+            finalsum = operators(+currentValue,+preValue,operate)
+            console.log(finalsum)
+            preValue = finalsum
+            currentValue = null
+            console.log(currentValue)
+        } else {
+            preValue = currentValue
+            console.log(currentValue)
         }
-        
+         operate = e.target.textContent
+         print.textContent = " "
+         print.innerText = finalsum
+        // console.log(item.textContent)
     })
 })
 
@@ -106,39 +94,25 @@ symbol.forEach((item)=>{
 
 
 result.addEventListener("click",(e)=>{
-    let store = print.innerText
-    arr.push(store)
-    let Arr =  arr.filter((e)=>{
-            return e
-    })
-    if (currentOperator  === "×"){
-    let product  =  Arr.reduce((acc,item)=>{
-        return acc*item
-    })
-    print.innerText = product
-    arr.length = 0
-    Arr.length = 0
-    console.log(arr)
-    } else if (currentOperator === "+"){
-        let sum = Arr.reduce((acc,item)=>{
-            return +acc +  +item
-        },0)
-        print.innerText = sum
-        arr.length = 0
-        Arr.length = 0
-    } else if(currentOperator ==="÷"){
-        return false
-    } else if (currentOperator === "-"){
-        let subtrace = Arr.reduce((acc,item)=>{
-            return acc - item
-        })
-        print.innerText = subtrace
-        arr.length = 0
-        Arr.length = 0
-    }
-    
+    finalsum = operators(+currentValue, +preValue, operate)
+    print.innerText = finalsum
 })
 
+
+
+function operators(a, b, operator) {
+  if (operator === '+') return a + b;
+  else if (operator === '-') return b - a;
+  else if (operator === '×') return a * b;
+  else if (operator === '÷') {
+    if (b === 0) {
+      alert("Cannot divide by zero");
+      return a; 
+    }
+    return a / b;
+  }
+  return b;
+}
 
 
 
@@ -157,15 +131,4 @@ result.addEventListener("click",(e)=>{
 // }
 // factorial(6)
 
-function operate(){
-    number = 5 
-    anotherNumber = 6 
-    if (operator === "+"){
-        add(number,anotherNumber)
-    } else if (operator === "-"){
-        subtract(number,anotherNumber)
-    } else if (operator === "!"){
-       
-    }
-}
 
